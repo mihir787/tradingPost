a1aa4bffffca7baaf7a74448d57d7ca7bd4f5f0f
Mihir Parikh <mihir787@gmail.com>
2015-08-01 22:13:38 -0600

    finished sessions

--- spec/api/v1/sessions_controller_spec.rb
+++ spec/api/v1/sessions_controller_spec.rb
@@ -4,36 +4,25 @@ RSpec.describe "Api::V1::SessionsController" do
   it 'can login' do
     user = User.create(:name => "kyle", :email => "kyle787@gmail.com", :password => "password", :password_confirmation => "password")
 
-    user_params = {:email => "kyle787@gmail.com", :password => "password", :password_confirmation => "password"}
+    user_params = {:email => "kyle787@gmail.com", :password => "password"}
 
     post '/api/v1/sessions', format: :json, user: user_params
     expect(response.status).to be(201)
   end
 
-  # it 'can show a user' do
-  #   user = User.create(:name => "mihir", :email => "mihir787@gmail.com", :password => "password", :password_confirmation => "password")
-  #
-  #   get "/api/v1/users/#{user.id}", format: :json
-  #
-  #   body = JSON.parse(response.body)
-  #   expect(response.status).to eq 200
-  #   expect(body["name"]).to eq "mihir"
-  # end
-  #
-  # it 'can update a user' do
-  #   user = User.create(:name => "mihir", :email => "mihir787@gmail.com", :password => "password", :password_confirmation => "password")
-  #
-  #   get "/api/v1/users/#{user.id}", format: :json
-  #
-  #   body = JSON.parse(response.body)
-  #   expect(response.status).to eq 200
-  #   expect(body["name"]).to eq "mihir"
-  #
-  #   user_params = {:name => "mihir p", :email => "mihir787@gmail.com", :password => "password", :password_confirmation => "password"}
-  #
-  #   put "/api/v1/users/#{user.id}", format: :json, user: user_params
-  #
-  #   expect(response.status).to eq 204
-  #   expect(User.first.name).to eq "mihir p"
-  # end
+  it 'can logout' do
+    user = User.create(:name => "kyle", :email => "kyle787@gmail.com", :password => "password", :password_confirmation => "password")
+
+    user_params = {:email => "kyle787@gmail.com", :password => "password"}
+
+    post '/api/v1/sessions', format: :json, user: user_params
+    expect(response.status).to be(201)
+
+
+    delete "/api/v1/sessions/#{user.id}", format: :json, user: user_params
+    expect(response.status).to be(204)
+  end
+
+
+
 end
